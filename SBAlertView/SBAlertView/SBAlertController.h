ec0ebe819657aeebe4d522009dee5ce2f83f2f17
subing-local <>
2018-06-19 22:58:27 +0800

    注释

--- SBAlertView/SBAlertView/SBAlertController.h
+++ SBAlertView/SBAlertView/SBAlertController.h
@@ -10,18 +10,22 @@
 #import "SBAlertDelegate.h"
 #import "UIView+SBAlertItem.h"
 
+/**
+ AlertView整体的控制器， 掌控所有Alert的显示和隐藏 ，本身是一个单例
+ */
 @interface SBAlertController : NSObject
 
+// 是否有 Alert正在显示
 @property (nonatomic , getter=isVisible) BOOL visible;
-
+//  当前正在显示的AlertView ，没有显示时 nil
 @property (nonatomic,readonly) id <SBAlertDelegate> alertView;
-
+// 通过 style 生成Alert
 + (id <SBAlertDelegate>)alertViewWithStyle:(UIAlertControllerStyle)style;
-
+// 单例
 + (instancetype)shareAlert;
-
+// 展示特定的 alertview
 - (void)showAlert:(id <SBAlertDelegate>)alertView;
-
+//使正在显示的Alert消失
 - (void)dissmiss;
 
 @end
--- SBAlertView/SBAlertView/SBAlertDelegate.h
+++ SBAlertView/SBAlertView/SBAlertDelegate.h
@@ -13,19 +13,19 @@
 @protocol SBAlertDelegate 
 
 @required
-
+//展示方式  ， alert 或 sheet 参照 UIAlertController
 @property (nonatomic,readonly) UIAlertControllerStyle alertStyle;
-
+//是否显示 因为Alert存在叠加情况 所以这个属性是必须的
 @property (nonatomic , getter=isVisible) BOOL visible;
-
+// 内容 item  ，alertview通过items作为内容完成自动布局
 @property (nonatomic) NSArray <UIView *> *items;
-
+//布局
 - (void)reloadData;
-
+// 消除动画和完成回调
 - (void)dissmissAnimationWithCompleted:(void(^)(id <SBAlertDelegate>  alert))completedBlock;
-
+// 展示动画和完成回调
 - (void)showAnimationWithCompleted:(void(^)(id <SBAlertDelegate> alert))completedBlock;
-
+// 手动调动 来消除这个  Alert
 - (void)dissmiss;
 
 @end
